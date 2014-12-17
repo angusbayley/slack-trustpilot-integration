474c6df25b5976ffd5a7780f67a2d6d8d45d6c91
Angus Bayley <angusbayley@gmail.com>
2014-12-17 22:43:36 +0000

    adds Nokogiri, messy debug comments too

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "rest_client"
+gem 'nokogiri', '1.6.5'
--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'rest_client'
 require 'json'
+require 'nokogiri'
 
 # Pull API key from the environment
 API_URL = "https://hooks.slack.com"
@@ -19,12 +20,21 @@ end
 
 # Trsutpilot Webhook
 post '/trustpilot-webhook' do
-  text = params["body-plain"]
+  text = params["body-html"]
+  puts "Angus Bayley, wise sage."
+  puts "\n\n\n\n\n\n\n\n"
+  puts text
+  puts "\n\n\n\n\n\n\n\n"
+  puts "Angus Bayley, wise sage."
+  doc = Nokogiri::HTML(text)
+  puts doc.xpath("//div")
+  puts "Angus, Wisest of sages"
   payload = {
   	"channel" => "#slack-testing",
-  	"username" => "pilot",
+  	"username" => "slack-trustpilot-integration",
   	"text" => text,
   	"icon_emoji" => ":ghost:"
   }
-  API[SLACK_PATH].post(payload.to_json)
+  #API[SLACK_PATH].post(payload.to_json)
+  puts "Angus, Sage, Much Wise"
 end
