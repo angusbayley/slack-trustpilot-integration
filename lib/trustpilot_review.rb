b46da1b3d3c34bcfd75ff6e24f6ed32c02949015
Angus Bayley <angusbayley@gmail.com>
2015-01-28 19:33:31 +0000

    tidying up by removing some comments and puts

--- lib/trustpilot_review.rb
+++ lib/trustpilot_review.rb
@@ -6,18 +6,15 @@ class TrustpilotReview
 
   def overview
     raw_overview = ptags[0].to_s
-    #puts "overview: #{format_overview(raw_overview)}"
     format_overview(raw_overview)
   end
 
   def quote
     raw_quote = ptags[0].to_s + ptags[1].to_s
-    puts "quote: #{format_quote(raw_quote)}".inspect
     format_quote(raw_quote)
   end
 
   def link
-    #puts "link: <#{link_url}|see the review here>"
     "<#{link_url}|see the review here>"
   end
 
@@ -36,7 +33,6 @@ class TrustpilotReview
   end
 
   def format_overview(overview)
-    #puts "raw_overview: #{overview}"
     overview = overview.sub("<p>","").
           sub("</p>","").
           sub("<strong>","*").
@@ -46,7 +42,6 @@ class TrustpilotReview
   end
 
   def format_quote(quote)
-    puts "raw_quote: #{quote}".inspect
     quote = quote.gsub(/\r/, "").                 # removing other line breaks
             gsub(/\t/, "").gsub("    ", "")       # removing tabs
     quote = quote[quote.index("<br>") + "<br>".length..-1]
@@ -65,44 +60,3 @@ class TrustpilotReview
   end
 
 end
-
-# ANGUS: You'll want to start here
-# I'd move the below into helper methods and use them to complete the main methods defined
-# above.
-#
-# def parse(html)
-#   p_tags = doc.xpath("//div//p").to_a
-#   a_tags = doc.xpath("//a").to_a
-#
-#   overview = p_tags[0].to_s
-#   review_body = p_tags[1].to_s
-#   link = a_tags[0].to_s
-#
-#   review = {
-#     overview => format_overview(overview),
-#     review_body => format_review_body(review_body),
-#     link => format_link(link)
-#   }
-# end
-# def format_message(message)
-#   message = message.sub("<p>","").
-#         sub("</p>","").
-#         sub("<strong>","*").
-#         sub("</strong>","*").
-#         strip
-#   message[0, message.index('<br>')]
-# end
-#
-# def format_review(review)
-#   review = review.sub("<p>","").
-#         sub("</p>","").
-#         strip
-#   review.insert(0, '_"')
-#   review.insert(review.length, '"_')
-# end
-#
-# def format_link(link)
-#   link.sub!('<a href="',"")
-#   link = link[0, link.index('"')]
-#   "<#{link}|see the review here>"
-# end
